1c5f2e3398ecd7f7cf717c63c8d1ed161c7eb508
Xuan Phuc PHAM <101117469+phuctheempire@users.noreply.github.com>
2024-11-30 17:30:48 +0100

    Update log_test_mdp.php

--- www/log_test_mdp.php
+++ www/log_test_mdp.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à la base de données
-$servername = "db";
+$servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "insa_db";
@@ -11,17 +11,20 @@ if ($conn->connect_error) {
 }
 
 // Charger le dictionnaire
-$dictionnaire = file("/var/www/html/dict1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$dictionnaire = file("dict1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 // Récupérer les mots de passe des utilisateurs
 $sql = "SELECT id, email, password FROM Users";
 $result = $conn->query($sql);
+$file = "hashes.txt";
+$handle = fopen($file, "w");
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $user_id = $row['id'];
         $email = $row['email'];
         $password = $row['password'];
+        fwrite($handle, $row['email'] . ":" . $row['password'] . "\n");
 
         // Test avec le dictionnaire
         $is_weak = false;
@@ -34,20 +37,34 @@ if ($result->num_rows > 0) {
             }
 
             // Vérification pour les mots de passe hachés
-            if (password_verify($word, $password)) {
-                echo "Mot de passe faible (haché) trouvé pour l'utilisateur $email (ID: $user_id): $word\n";
-                $is_weak = true;
-                break;
-            }
+            // if (password_verify($word, $password)) {
+            //     echo "Mot de passe faible (haché) trouvé pour l'utilisateur $email (ID: $user_id): $word\n";
+            //     $is_weak = true;
+            //     break;
+            // }
+
         }
 
         if (!$is_weak) {
             echo "Mot de passe robuste pour l'utilisateur $email (ID: $user_id)\n";
+            $command = "john --wordlist=dict1.txt hashes.txt";
+            $output = shell_exec($command);
+            echo "<pre>$output</pre>"; // Affiche la sortie brute de John the Ripper
+
+            // Récupérer les résultats des mots de passe craqués
+            $showCommand = "john --show hashes.txt";
+            $crackedPasswords = shell_exec($showCommand);
+            echo "<pre>Mots de passe craqués :\n$crackedPasswords</pre>";
         }
+
+       
     }
 } else {
     echo "Aucun utilisateur trouvé.";
 }
+
+fclose($handle);
+echo "Fichier $file généré.";
 
 // Fermer la connexion
 $conn->close();
