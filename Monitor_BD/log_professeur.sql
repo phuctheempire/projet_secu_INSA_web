09ec33eed3111402ee557f559422c4d28fc7c9a0
yoannconstans <145764040+yoannconstans@users.noreply.github.com>
2024-12-08 07:10:57 -0500

    Create log_professeur.sql

---
+++ Monitor_BD/log_professeur.sql
@@ -0,0 +1,10 @@
+CREATE TABLE LOG_Professeur (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    operation_type VARCHAR(10), -- Type de l'opération (INSERT, UPDATE, DELETE)
+    table_name VARCHAR(50),    -- Nom de la table (ici "Professeur")
+    row_id INT,                -- ID de la ligne affectée
+    changed_fields TEXT,       -- Champs modifiés (nom des colonnes)
+    old_values TEXT,           -- Anciennes valeurs (JSON)
+    new_values TEXT,           -- Nouvelles valeurs (JSON)
+    operation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
