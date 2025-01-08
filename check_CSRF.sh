1df43cfac40dbdbd6bb49c16e28f477c004dbb3b
yoannconstans <145764040+yoannconstans@users.noreply.github.com>
2025-01-08 10:33:30 +0100

    Update check_CSRF.sh

--- check_CSRF.sh
+++ check_CSRF.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+source ./check_login.sh
+
+check_login
+
+# Vérifier si le fichier de cookies a été généré
+if [ ! -f $COOKIE_FILE ]; then
+    echo "Erreur : le fichier de cookies n'a pas été généré. Impossible de continuer."
+    exit 1
+fi
+
 # URL de la page qui gère l'envoi de mails
-URL="http://127.0.0.1/functions/user/mail.php"
+URL="http://127.0.0.1/controller/api/user/mail.php"
 
 # Données malveillantes
 DATA="receiver_emails=hacker@gmail.com&title=Bravo&content=<a href='https://www/functions/user/mail.php?id=3&new_email=hacker2@gmail.com'>Cliquez ici pour un cadeau!</a>&send_email=1"
