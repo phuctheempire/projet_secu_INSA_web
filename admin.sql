852c6c2626d52e260989ba9d984d7caa381d461a
yoannconstans <145764040+yoannconstans@users.noreply.github.com>
2025-01-23 16:52:40 +0100

    Update admin.sql

--- admin.sql
+++ admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO `Users` (`id`, `email`, `password`, `nom`, `prenom`, `sexe`, `date_naissance`, `adresse`, `telephone`, `image_path`, `role`) VALUES
-(`0`, `admin@insa-cvl.fr`, `admin`, `admin`, `admin`, `M`, `1990-01-01`, `1 rue de la Loire`, `0601020304`, ``, `admin`);
+('0', 'admin@insa-cvl.fr', 'admin', 'admin', 'admin', 'M', '1990-01-01', '1 rue de la Loire', '0601020304', ``, 'admin');
